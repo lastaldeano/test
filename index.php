8017d8c8ff4790695f2d9110a1ffc8e0d31977da
Jesus Lazaro Suarez Perez <last.aldeano@gmail.com>
2017-03-15 10:45:13 -0500

    devolviendo mensage con botones

--- index.php
+++ index.php
@@ -52,10 +52,54 @@
             ]
         ],
         'contextOut' => [],
-        'source' => '',
+        'source' => 'DuckDuckGo',
         'followupEvent' => [],
 
     ];
     header('Content-Type: application/json');
-    echo json_encode($response);
+    echo '{
+"speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+"displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+"data": '.json_encode([
+            'slack' => [
+                "text" => 'I am a test message http://slack.com',
+                "attachments" => [
+                    "text" => "Choose a game to play",
+                    "fallback" => "You are unable to choose a game",
+                    "callback_id" => "wopr_game",
+                    "color" => "#3AA3E3",
+                    "attachment_type" => "default",
+                    "actions" => [
+                        [
+                            "name" => "game",
+                            "text" => "Chess",
+                            "type" => "button",
+                            "value" => "chess"
+                        ],
+                        [
+                            "name" => "game",
+                            "text" => "Falken's Maze",
+                            "type" => "button",
+                            "value" => "maze"
+                        ],
+                        [
+                            "name" => "game",
+                            "text" => "Thermonuclear War",
+                            "style" => "danger",
+                            "type" => "button",
+                            "value" => "war",
+                            "confirm" => [
+                                "title" => "Are you sure?",
+                                "text" => "Wouldn't you prefer a good game of chess?",
+                                "ok_text" => "Yes",
+                                "dismiss_text" => "No"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]).',
+"contextOut": [...],
+"source": "DuckDuckGo"
+}';
 ?>
