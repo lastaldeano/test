0cf208a2bf15f5dff2285cb191d5d4d3869fb788
Jesus Lazaro Suarez Perez <last.aldeano@gmail.com>
2017-03-15 09:25:57 -0500

    devolviendo mensage con botones

--- index.php
+++ index.php
@@ -1,1 +1,64 @@
-<?php echo var_dump($_POST); ?>
+<?php
+/*if(getallheaders()['HEADERS_BUTTONS'])
+{
+    echo json_encode([
+
+    ]);
+}*/
+
+
+//file_put_contents('log.txt', '<br><pre>' . print_r($_POST) . '</pre>', FILE_APPEND);
+if (isset($_POST['sessionId'])) {
+    $response = [
+        'speech' => 'test text',
+        'displayText' => 'test text',
+        'data' => [
+            'slack' => [
+                "text" => 'I am a test message http://slack.com',
+                "attachments" => [
+                    "text" => "Choose a game to play",
+                    "fallback" => "You are unable to choose a game",
+                    "callback_id" => "wopr_game",
+                    "color" => "#3AA3E3",
+                    "attachment_type" => "default",
+                    "actions" => [
+                        [
+                            "name" => "game",
+                            "text" => "Chess",
+                            "type" => "button",
+                            "value" => "chess"
+                        ],
+                        [
+                            "name" => "game",
+                            "text" => "Falken's Maze",
+                            "type" => "button",
+                            "value" => "maze"
+                        ],
+                        [
+                            "name" => "game",
+                            "text" => "Thermonuclear War",
+                            "style" => "danger",
+                            "type" => "button",
+                            "value" => "war",
+                            "confirm" => [
+                                "title" => "Are you sure?",
+                                "text" => "Wouldn't you prefer a good game of chess?",
+                                "ok_text" => "Yes",
+                                "dismiss_text" => "No"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ],
+        'contextOut' => [],
+        'source' => '',
+        'followupEvent' => [],
+
+    ];
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+
+?>
