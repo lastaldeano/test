3748afdd1bf8dab1babb2285fe20e5d032862bf7
Jesus Lazaro Suarez Perez <last.aldeano@gmail.com>
2017-03-16 12:48:33 -0500

    devolviendo mensage con botones

--- index.php
+++ index.php
@@ -3,49 +3,12 @@
     header('Content-Type: application/json');
 file_put_contents("php://output",
 '{
-"speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
-"displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-"data": {"slack": {
-     "text": "Would you like to play a game?",
-    "attachments": [
+      "speech": "Hi Sam! Nice to meet you!",
+      "messages": [
         {
-            "text": "Choose a game to play",
-            "fallback": "You are unable to choose a game",
-            "callback_id": "wopr_game",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "game",
-                    "text": "Chess",
-                    "type": "button",
-                    "value": "chess"
-                },
-                {
-                    "name": "game",
-                    "text": "Falken\'s Maze",
-                    "type": "button",
-                    "value": "maze"
-                },
-                {
-                    "name": "game",
-                    "text": "Thermonuclear War",
-                    "style": "danger",
-                    "type": "button",
-                    "value": "war",
-                    "confirm": {
-                        "title": "Are you sure?",
-                        "text": "Wouldn\'t you prefer a good game of chess?",
-                        "ok_text": "Yes",
-                        "dismiss_text": "No"
-                    }
-                }
-            ]
+          "type": 0,
+          "speech": "Hi Sam! Nice to meet you!"
         }
-    ]
-}}
-"contextOut": [],
-"source": "testagent"
-} '
-);
+      ]
+    }');
 ?>
