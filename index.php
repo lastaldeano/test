22908ddab73a2b11f3fdd8b2e55d3078a0320d35
Jesus Lazaro Suarez Perez <last.aldeano@gmail.com>
2017-03-16 11:30:10 -0500

    1

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ file_put_contents("php://output",
 "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
 "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
 "data": {"slack": {
-
      "text": "Would you like to play a game?",
     "attachments": [
         {
