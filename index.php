f14cc9f53ce5bcfcf96360704e42f113a592c12a
Jesus Lazaro Suarez Perez <last.aldeano@gmail.com>
2017-03-16 11:16:36 -0500

    devolviendo mensage con botones

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
 
     header('Content-Type: application/json');
-file_put_contents("php://output", '
-    "speech": "Seleccione la pregunta mas parecida a la suya.\n1. ¿Cuánta ayuda económica puedo recibir?\n2. ¿Cuánta ayuda económica puedo recibir?\n3. ¿Reúno los requisitos para recibir ayuda económica?\n4. ¿Reúno los requisitos para recibir ayuda económica?\n5. FAFSA: Solicitar ayuda económica » Próximos pasos » Recibir ayuda económica\n",
-      "source": "testagent",
+file_put_contents("php://output",
+    /*'{"speech": "Seleccione la pregunta mas parecida a la suya.\n1. ¿Cuánta ayuda económica puedo recibir?\n2. ¿Cuánta ayuda económica puedo recibir?\n3. ¿Reúno los requisitos para recibir ayuda económica?\n4. ¿Reúno los requisitos para recibir ayuda económica?\n5. FAFSA: Solicitar ayuda económica » Próximos pasos » Recibir ayuda económica\n",
       "displayText": "Seleccione la pregunta mas parecida a la suya.\n1. ¿Cuánta ayuda económica puedo recibir?\n2. ¿Cuánta ayuda económica puedo recibir?\n3. ¿Reúno los requisitos para recibir ayuda económica?\n4. ¿Reúno los requisitos para recibir ayuda económica?\n5. FAFSA: Solicitar ayuda económica » Próximos pasos » Recibir ayuda económica\n",
+      "data": {},
+      "contextOut": [],
+      "source": "webhook",
       "messages": [
         {
           "type": 0,
           "speech": "Seleccione la pregunta mas parecida a la suya.\n1. ¿Cuánta ayuda económica puedo recibir?\n2. ¿Cuánta ayuda económica puedo recibir?\n3. ¿Reúno los requisitos para recibir ayuda económica?\n4. ¿Reúno los requisitos para recibir ayuda económica?\n5. FAFSA: Solicitar ayuda económica » Próximos pasos » Recibir ayuda económica\n"
         }
-      ]
-    ');
+      ]}'*/
+'{
+"speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+"displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+"data": {},
+"contextOut": [],
+"source": "testagent"
+}'
+
+);
 ?>
