feb5d4f8f6417746196a8ce4969ff157a1e4ff0a
Jesus Lazaro Suarez Perez <last.aldeano@gmail.com>
2017-03-16 11:36:49 -0500

    devolviendo mensage con botones

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    /* test */
     header('Content-Type: application/json');
 file_put_contents("php://output",
     /*'{"speech": "Seleccione la pregunta mas parecida a la suya.\n1. ¿Cuánta ayuda económica puedo recibir?\n2. ¿Cuánta ayuda económica puedo recibir?\n3. ¿Reúno los requisitos para recibir ayuda económica?\n4. ¿Reúno los requisitos para recibir ayuda económica?\n5. FAFSA: Solicitar ayuda económica » Próximos pasos » Recibir ayuda económica\n",
